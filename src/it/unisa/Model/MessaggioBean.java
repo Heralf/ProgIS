5d423f92fe8e424fc618488386b410c9db671457
Adamo Coppola <32262332+Heralf@users.noreply.github.com>
2018-02-01 18:48:24 +0100

    MessaggioBean
    aggiunti commenti

--- src/it/unisa/Model/MessaggioBean.java
+++ src/it/unisa/Model/MessaggioBean.java
@@ -10,42 +10,42 @@ public class MessaggioBean implements Serializable{
 	String Titolo;
 	String Messaggio;
 	Boolean Visto;
-	
+	//funzione che inizializza tutte le variabili
 	public MessaggioBean(){
 		IDProprietario = 0;
 		Titolo = "";
 		Messaggio = "";
 		Visto = false;
 	}
-	
+	//sostituisce la variabile intera IDProprietario con quella ottenuta
 	public void setIDProprietario(int IDProprietario){
 		this.IDProprietario = IDProprietario;
 	}
-	
+	//restituisce la variabile itera IDProprietario
 	public int getIDProprietario(){
 		return IDProprietario;
 	}
-	
+	//sostituisce la variabile stringa Titolo con quella ottenuta
 	public void setTitolo(String Titolo){
 		this.Titolo = Titolo;
 	}
-	
+	//restituisce la variabile stringa Titolo
 	public String getTitolo(){
 		return Titolo;
 	}
-	
+	//sostituisce la variabile stringa Messaggio con quella ottenuta
 	public void setMessaggio(String Messaggio){
 		this.Messaggio = Messaggio;
 	}
-	
+	//restituisce la variabile stringa Messagio
 	public String getMessaggio(){
 		return Messaggio;
 	}
-	
+	//sostituisce la variabile Booleana Visto con quella ottenuta
 	public void setVisto(Boolean Visto){
 		this.Visto = Visto;
 	}
-	
+	//restituisce la variabile Booleana Visto
 	public boolean getVisto(){
 		return Visto;
 	}
